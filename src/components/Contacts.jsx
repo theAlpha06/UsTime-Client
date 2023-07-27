12e8aa94d1fa86cc26e7ade96e791e30e28a8513
Shubham <95537249+theAlpha06@users.noreply.github.com>
2023-07-27 20:16:22 +0530

    if avatar then render

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -39,22 +39,24 @@ function Contacts({ contacts, currentUser, changeChat, handleResponsive }) {
         {
           users.map((user, index) => {
             return (
-              <div className={`contacts_list ${index === currentSelected ? "selectedChat" : ""}`}
-                key={index}
-                onClick={() => {
-                  handleResponsive();
-                  currentChat(index, user);
-                }}
-              >
-                <div className='contacts_avatar'>
-                  <img src={`data:image/svg+xml;base64,${user.avatarImage}`} alt=''
-                  width={50}
-                  />
+              user.avatarImage && (
+                <div className={`contacts_list ${index === currentSelected ? "selectedChat" : ""}`}
+                  key={index}
+                  onClick={() => {
+                    handleResponsive();
+                    currentChat(index, user);
+                  }}
+                >
+                  <div className='contacts_avatar'>
+                    <img src={`data:image/svg+xml;base64,${user.avatarImage}`} alt=''
+                      width={50}
+                    />
+                  </div>
+                  <div className='contacts_name'>
+                    {user.name}
+                  </div>
                 </div>
-                <div className='contacts_name'>
-                  {user.name}
-                </div>
-              </div>
+              )
             )
 
           })
