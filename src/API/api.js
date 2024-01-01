c3a7c8e263c8f2e7202e399ca864d8f0956c5ef4
Shubham Kumar Singh <95537249+theAlpha06@users.noreply.github.com>
2024-01-01 15:17:37 +0530

    Update api.js

--- src/API/api.js
+++ src/API/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // export const baseUrl = 'http://localhost:4000';
-export const baseUrl = 'https://ustime.onrender.com';
+export const baseUrl = 'http://191.96.57.27:4000';
+// export const baseUrl = 'https://ustime.onrender.com';
 
 export const localUrlIns = axios.create({
   baseURL: baseUrl,
